e0ed7b0f3a2b17eda1723601e1df6aa1c6851690
erice <erice@ENETLAPTOP>
2024-10-12 15:08:28 +1000

    Add project files.

---
+++ ee.blazor.stock-price.Data/Stock.cs
@@ -0,0 +1,11 @@
+﻿namespace ee.blazor.stock_price.Data
+{
+    public class Stock
+    {
+        public string id { get; set; }
+        public string stockId { get; set; }
+        public string symbol { get; set; }
+        public decimal price { get; set; }
+        public string timestamp { get; set; }
+    }
+}
